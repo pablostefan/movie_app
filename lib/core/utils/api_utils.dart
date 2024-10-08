dfe556763704d0ffd8f6e83d9c0182f9d6d84535
Pablo Stefan Gomes Pereira <pablo.stefan.dev@gmail.com>
2024-10-07 22:58:16 -0300

    feat: add .env file with API credentials
    - Added .env file to store sensitive API information, including tokens and keys.
    - Updated .gitignore to exclude .env from version control for security.
    - Loaded .env in main.dart to access environment variables throughout the app.

--- lib/core/utils/api_utils.dart
+++ lib/core/utils/api_utils.dart
@@ -1,10 +1,9 @@
 class API {
-  static String imageLink(String img) => 'https://image.tmdb.org/t/p/w500/$img';
-  static const String baseUrl = 'https://api.themoviedb.org/3/';
-  static const String trendingMovies = 'trending/movie/week';
-  static const String searchMovies = 'search/movie';
+  static String imageLink(String img) => '${const String.fromEnvironment("IMAGE_URL")}/$img';
+  static const String baseUrl = String.fromEnvironment("BASE_URL");
+  static const String trendingMovies = String.fromEnvironment("TRENDING_MOVIES_URL");
+  static const String searchMovies = String.fromEnvironment("SEARCH_MOVIES_URL");
   static const Map<String, String> headers = {
-    'authorization':
-        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNzhlNmIxODMwODQ3MmMxMmQ2YWUwNDc5MTliY2RiZiIsIm5iZiI6MTcyODA5NTY0OC4zMTA1NDYsInN1YiI6IjY0NTEzZDlhNDM1MDExMDBlYTNhMTk1ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.J2Gju5G6gE0_Twm-hVIpwGQxydd-utKgFlzV9rPYTZE'
+    'authorization': 'Bearer ${const String.fromEnvironment("BEARER_TOKEN")}'
   };
 }
