1fbe7f16b84b283c9017445b87128be85cdcc576
Pablo Stefan Gomes Pereira <pablo.stefan.dev@gmail.com>
2024-10-08 01:35:47 -0300

    test(datasources): add unit tests for MoviesLocalDataSourceDecoratorImp and MoviesRemoteDatasourceImp
    - Added unit tests to validate trending and search movie retrieval with caching
    - Included tests to handle network failures, using cached data as fallback when available
    - Verified CacheDataFailure handling when both network and cache are unavailable

--- test/test_unit/data/local/local_movies_datasource_test.dart
+++ test/test_unit/data/local/local_movies_datasource_test.dart
@@ -1,75 +1,78 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:movie_app/core/error/base_failure.dart';
-import 'package:movie_app/features/movies/data/datasources/local/movies_local_datasource_decorator_imp.dart';
-import 'package:movie_app/features/movies/data/dtos/the_movie_db_dto.dart';
+import 'package:movie_app/core/utils/api_utils.dart';
+import 'package:movie_app/features/movies/data/datasources/remote/movies_remote_datasource_imp.dart';
 import 'package:movie_app/features/movies/domain/entities/the_movie_db_entity.dart';
 
-import 'local_movies_datasource_test.mocks.dart';
+import '../remote/remote_movies_datasource_test.mocks.dart';
 
 void main() {
-  late MoviesLocalDataSourceDecoratorImp dataSourceDecorator;
-  late MockLocalStorageService mockLocalStorageService;
-  late MockMoviesDataSource mockMoviesDataSource;
+  late MoviesRemoteDatasourceImp dataSource;
+  late MockHttpService mockHttpService;
 
   setUp(() {
-    mockLocalStorageService = MockLocalStorageService();
-    mockMoviesDataSource = MockMoviesDataSource();
-    dataSourceDecorator = MoviesLocalDataSourceDecoratorImp(mockMoviesDataSource, mockLocalStorageService);
+    mockHttpService = MockHttpService();
+    dataSource = MoviesRemoteDatasourceImp(mockHttpService);
   });
 
-  group('MoviesLocalDataSourceDecoratorImp', () {
-    test('Should return TheMovieDbEntity when calling getTrendingMovies and store it in cache', () async {
-      final movieDbEntity = TheMovieDbEntity(page: 1, totalPages: 1, totalResults: 1, results: []);
-      const cacheKey = 'trending_movies_1';
+  group('MoviesRemoteDatasourceImp', () {
+    test('Must return TheMovieDbEntity when calling getTrendingMovies', () async {
+      final response = Response(
+        requestOptions: RequestOptions(path: ''),
+        data: {"results": []},
+        statusCode: 200,
+      );
 
-      when(mockMoviesDataSource.getTrendingMovies(page: 1)).thenAnswer((_) async => movieDbEntity);
-      when(mockLocalStorageService.putData(cacheKey, movieDbEntity.toJson())).thenAnswer((_) async => true);
+      when(mockHttpService.get(API.trendingMovies, queryParameters: anyNamed('queryParameters')))
+          .thenAnswer((_) async => response);
 
-      final result = await dataSourceDecorator.getTrendingMovies(page: 1);
-
+      final result = await dataSource.getTrendingMovies(page: 1);
       expect(result, isA<TheMovieDbEntity>());
-      verify(mockMoviesDataSource.getTrendingMovies(page: 1)).called(1);
-      verify(mockLocalStorageService.putData(cacheKey, movieDbEntity.toJson())).called(1);
+      verify(mockHttpService.get(API.trendingMovies, queryParameters: anyNamed('queryParameters'))).called(1);
     });
 
-    test('Should return TheMovieDbEntity when calling getSearchMovies and store it in cache', () async {
-      final movieDbEntity = TheMovieDbEntity(page: 1, totalPages: 1, totalResults: 1, results: []);
-      const cacheKey = 'search_movies_1_some_query';
+    test('Must return TheMovieDbEntity when calling getSearchMovies', () async {
+      final response = Response(
+        requestOptions: RequestOptions(path: ''),
+        data: {"results": []},
+        statusCode: 200,
+      );
 
-      when(mockMoviesDataSource.getSearchMovies(page: 1, query: 'some_query')).thenAnswer((_) async => movieDbEntity);
-      when(mockLocalStorageService.putData(cacheKey, movieDbEntity.toJson())).thenAnswer((_) async => true);
-      when(mockLocalStorageService.putString(any, any)).thenAnswer((_) async => true);
+      when(mockHttpService.get(API.searchMovies, queryParameters: anyNamed('queryParameters')))
+          .thenAnswer((_) async => response);
 
-      final result = await dataSourceDecorator.getSearchMovies(page: 1, query: 'some_query');
-
+      final result = await dataSource.getSearchMovies(page: 1, query: 'some_query');
       expect(result, isA<TheMovieDbEntity>());
-      verify(mockMoviesDataSource.getSearchMovies(page: 1, query: 'some_query')).called(1);
-      verify(mockLocalStorageService.putData(cacheKey, movieDbEntity.toJson())).called(1);
-      verify(mockLocalStorageService.putString(any, any)).called(1);
+      verify(mockHttpService.get(API.searchMovies, queryParameters: anyNamed('queryParameters'))).called(1);
     });
 
-    test('Should return data from cache in case of NetworkFailure', () async {
-      const cacheKey = 'trending_movies_1';
-      final cachedData = TheMovieDbEntity(page: 1, totalPages: 1, totalResults: 1, results: []).toJson();
+    test('Should throw NetworkFailure when getTrendingMovies encounters a Dio error', () async {
+      when(mockHttpService.get(API.trendingMovies, queryParameters: anyNamed('queryParameters')))
+          .thenThrow(DioException(requestOptions: RequestOptions(path: '')));
 
-      when(mockMoviesDataSource.getTrendingMovies(page: 1)).thenThrow(NetworkFailure(message: ''));
-      when(mockLocalStorageService.getData(cacheKey)).thenAnswer((_) async => cachedData);
-
-      final result = await dataSourceDecorator.getTrendingMovies(page: 1);
-
-      expect(result, isA<TheMovieDbEntity>());
-      verify(mockMoviesDataSource.getTrendingMovies(page: 1)).called(1);
-      verify(mockLocalStorageService.getData(cacheKey)).called(1);
+      expect(() => dataSource.getTrendingMovies(page: 1), throwsA(isA<NetworkFailure>()));
     });
 
-    test('Should throw CacheDataFailure when there is no data in cache and network fails', () async {
-      const cacheKey = 'trending_movies_1';
+    test('Should throw RemoteDataFailure when getTrendingMovies encounters an unexpected exception', () async {
+      when(mockHttpService.get(API.trendingMovies, queryParameters: anyNamed('queryParameters')))
+          .thenThrow(Exception());
 
-      when(mockMoviesDataSource.getTrendingMovies(page: 1)).thenThrow(NetworkFailure(message: ''));
-      when(mockLocalStorageService.getData(cacheKey)).thenAnswer((_) async => null);
+      expect(() => dataSource.getTrendingMovies(page: 1), throwsA(isA<RemoteDataFailure>()));
+    });
 
-      expect(() => dataSourceDecorator.getTrendingMovies(page: 1), throwsA(isA<CacheDataFailure>()));
+    test('Should throw NetworkFailure when getSearchMovies encounters a Dio error', () async {
+      when(mockHttpService.get(API.searchMovies, queryParameters: anyNamed('queryParameters')))
+          .thenThrow(DioException(requestOptions: RequestOptions(path: '')));
+
+      expect(() => dataSource.getSearchMovies(page: 1, query: 'some_query'), throwsA(isA<NetworkFailure>()));
+    });
+
+    test('Should throw RemoteDataFailure when getSearchMovies encounters an unexpected exception', () async {
+      when(mockHttpService.get(API.searchMovies, queryParameters: anyNamed('queryParameters'))).thenThrow(Exception());
+
+      expect(() => dataSource.getSearchMovies(page: 1, query: 'some_query'), throwsA(isA<RemoteDataFailure>()));
     });
   });
 }
